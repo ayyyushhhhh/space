ff0d5cabee3c0b755f9057cd24f5df1274e67abc
Ayush Rawat <ayush002rawat@gmail.com>
2023-01-09 22:01:59 +0530

    splash screen logo added

--- lib/provider/journal/journalProvider.dart
+++ lib/provider/journal/journalProvider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:space/models/journals/journal_model.dart';
 
--- lib/screens/journal/add_journal_screen.dart
+++ lib/screens/journal/add_journal_screen.dart
@@ -63,172 +63,160 @@ class _AddJournalScreenState extends State<AddJournalScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          color: Colors.grey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                color: Colors.grey,
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Container(
+              color: Colors.grey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        IconButton(
-                          padding: EdgeInsets.zero,
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(
-                            CupertinoIcons.back,
-                            size: 50.r,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                CupertinoIcons.back,
+                                size: 50.r,
+                              ),
+                            ),
+                            const Spacer(),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                _saveJournal();
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                Icons.done,
+                                size: 50.r,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          DateFormat('EEEE').format(createdOn),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30.sp,
                           ),
                         ),
-                        const Spacer(),
-                        // IconButton(
-                        //   padding: EdgeInsets.zero,
-                        //   onPressed: () {},
-                        //   icon: Icon(
-                        //     Icons.circle,
-                        //     size: 50.r,
-                        //     color: Colors.purple,
-                        //   ),
-                        // ),
-                        // const SizedBox(
-                        //   width: 10,
-                        // ),
-                        // IconButton(
-                        //   padding: EdgeInsets.zero,
-                        //   onPressed: () {},
-                        //   icon: Icon(
-                        //     Icons.add_reaction,
-                        //     size: 50.r,
-                        //   ),
-                        // ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        IconButton(
-                          padding: EdgeInsets.zero,
-                          onPressed: () {
-                            _saveJournal();
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(
-                            Icons.done,
-                            size: 50.r,
+                        Text(
+                          DateFormat('d, MMMM, yyyy').format(createdOn),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20.sp,
                           ),
+                        ),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        quill.QuillToolbar.basic(
+                          controller: _quillController,
+                          iconTheme: const quill.QuillIconTheme(
+                              iconSelectedColor: Colors.purpleAccent,
+                              iconSelectedFillColor: Colors.transparent),
+                          toolbarIconSize: 20.sp,
+                          showFontFamily: false,
+                          showFontSize: false,
+                          showListBullets: true,
+                          showCodeBlock: false,
+                          showBoldButton: true,
+                          showItalicButton: true,
+                          showUnderLineButton: true,
+                          showSearchButton: false,
+                          showLink: false,
+                          showListCheck: false,
+                          showAlignmentButtons: false,
+                          showInlineCode: false,
+                          showDirection: false,
+                          showJustifyAlignment: false,
+                          showDividers: false,
+                          showCenterAlignment: false,
+                          showBackgroundColorButton: false,
+                          showListNumbers: false,
+                          showIndent: false,
+                          showClearFormat: false,
+                          showColorButton: false,
+                          showLeftAlignment: false,
+                          multiRowsDisplay: false,
                         ),
                       ],
                     ),
-                    Text(
-                      DateFormat('EEEE').format(createdOn),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.sp,
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin:
+                          const EdgeInsets.only(top: 10, left: 10, right: 10),
+                      padding:
+                          const EdgeInsets.only(left: 10, right: 10, top: 10),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          topLeft: Radius.circular(30),
+                        ),
+                      ),
+                      child: quill.QuillEditor(
+                        controller: _quillController,
+                        readOnly: false,
+                        autoFocus: true,
+                        expands: true,
+                        focusNode: FocusNode(canRequestFocus: true),
+                        padding: const EdgeInsets.all(8.0),
+                        scrollController: _scrollController,
+                        scrollable: true,
                       ),
                     ),
-                    Text(
-                      DateFormat('d, MMMM, yyyy').format(createdOn),
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20.sp,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.h,
-                    ),
-                    quill.QuillToolbar.basic(
-                      controller: _quillController,
-                      iconTheme: const quill.QuillIconTheme(
-                          iconSelectedColor: Colors.purpleAccent,
-                          iconSelectedFillColor: Colors.transparent),
-                      toolbarIconSize: 20.sp,
-                      showFontFamily: false,
-                      showFontSize: false,
-                      showListBullets: true,
-                      showCodeBlock: false,
-                      showBoldButton: true,
-                      showItalicButton: true,
-                      showUnderLineButton: true,
-                      showSearchButton: false,
-                      showLink: false,
-                      showListCheck: false,
-                      showAlignmentButtons: false,
-                      showInlineCode: false,
-                      showDirection: false,
-                      showJustifyAlignment: false,
-                      showDividers: false,
-                      showCenterAlignment: false,
-                      showBackgroundColorButton: false,
-                      showListNumbers: false,
-                      showIndent: false,
-                      showClearFormat: false,
-                      showColorButton: false,
-                      showLeftAlignment: false,
-                      multiRowsDisplay: false,
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                  decoration: const BoxDecoration(
+                  ),
+                  Container(
+                    height: 50.h,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
                     color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      topLeft: Radius.circular(30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Mood",
+                          style: TextStyle(
+                              fontSize: 24.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                        ),
+                        _buildEmoji(emoji: "😀", mood: 'happy'),
+                        _buildEmoji(emoji: "😞", mood: 'sad'),
+                        _buildEmoji(emoji: "😐", mood: 'neutral'),
+                        _buildEmoji(emoji: "😤", mood: 'angry'),
+                        _buildEmoji(emoji: "😨", mood: 'worried'),
+                      ],
                     ),
-                  ),
-                  child: quill.QuillEditor(
-                    controller: _quillController,
-                    readOnly: false,
-                    autoFocus: true,
-                    expands: true,
-                    focusNode: FocusNode(canRequestFocus: true),
-                    padding: const EdgeInsets.all(8.0),
-                    scrollController: _scrollController,
-                    scrollable: true,
-                  ),
-                ),
+                  )
+                ],
               ),
-              Container(
-                height: 50.h,
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Mood",
-                      style: TextStyle(
-                          fontSize: 24.sp,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                    ),
-                    _buildEmoji(emoji: "😀", mood: 'happy'),
-                    _buildEmoji(emoji: "😞", mood: 'sad'),
-                    _buildEmoji(emoji: "😐", mood: 'neutral'),
-                    _buildEmoji(emoji: "😤", mood: 'angry'),
-                    _buildEmoji(emoji: "😨", mood: 'worried'),
-                  ],
-                ),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        ],
+      )),
     );
   }
 
